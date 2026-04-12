360ad61fe9f2a9411f916a1cc4901fb8d59c9e92
Turjo <126961528+turjo1237789@users.noreply.github.com>
2026-04-12 09:12:23 +0300

    Initialize Discord bot with Flask integration

---
+++ main.py
@@ -0,0 +1,54 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+from flask import Flask
+from flask import Flask, render_template
+import threading
+from utils import check_ban
+
+app = Flask(__name__)
+
+load_dotenv()
+APPLICATION_ID = os.getenv("APPLICATION_ID")
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+DEFAULT_LANG = "en"
+user_languages = {}
+
+nomBot = "None"
+
+@app.route('/')
+def home():
+    global nomBot
+    return f"Bot {nomBot} is working"
+    from datetime import datetime
+    return render_template('index.html', year=datetime.utcnow().year)
+
+def run_flask():
+    app.run(host='0.0.0.0', port=10000)
+
+threading.Thread(target=run_flask).start()
+
+@bot.event
+async def on_ready():
+    global nomBot
+    nomBot = f"{bot.user}"
+    print(f"Le bot est connecté en tant que {bot.user}")
+
+@bot.command(name="guilds")
+async def show_guilds(ctx):
+    guild_names = [f"{i+1}. {guild.name}" for i, guild in enumerate(bot.guilds)]
+    guild_list = "\n".join(guild_names)
+    await ctx.send(f"Le bot est dans les guilds suivantes :\n{guild_list}")
+
+@bot.command(name="lang")
+async def change_language(ctx, lang_code: str):
+    lang_code = lang_code.lower()
+    if lang_code not in ["en", "fr"]:
+        await ctx.send("❌ Invalid language. Available: `en`, `fr`")
+        return
